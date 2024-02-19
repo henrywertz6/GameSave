62be10455dbbcb44b71294c1f213bfabfd9653b6
Henry Wertz <hwertz@princeton.edu>
2024-02-19 13:59:21 -0500

    Initial Commit

---
+++ GameTracker/GameTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameTrackerApp.swift
+//  GameTracker
+//
+//  Created by Henry Wertz on 2/19/24.
+//
+
+import SwiftUI
+
+@main
+struct GameTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
